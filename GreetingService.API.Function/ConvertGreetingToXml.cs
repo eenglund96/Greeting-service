df1363402c35fb91067a9b0f3800e251c3a288d6
Englund Emelie (BT) <emelie.englund@hm.com>
2022-02-15 14:02:53 +0100

    Added an Xml Function

--- GreetingService.API.Function/ConvertGreetingToXml.cs
+++ GreetingService.API.Function/ConvertGreetingToXml.cs
@@ -9,21 +9,20 @@ using Microsoft.Extensions.Logging;
 
 namespace GreetingService.API.Function
 {
-    public class ConvertGreetingToCsv
+    public class ConvertGreetingToXml
     {
-        [FunctionName("ConvertGreetingToCsv")]
-        public async Task Run([BlobTrigger("greetings/{name}", Connection = "LogStorageAccount")]Stream greetingJsonBlob, string name, [Blob("greetings-csv/{name}", FileAccess.Write, Connection = "LogStorageAccount")] Stream greetingCsvBlob, ILogger log)
+        [FunctionName("ConvertGreetingToXml")]
+        public async Task Run([BlobTrigger("greetings/{name}", Connection = "LogStorageAccount")]Stream greetingJsonBlob, string name, [Blob("greetings-xml/{name}", FileAccess.Write)] Stream greetingXmlBlob, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {greetingJsonBlob.Length} Bytes");
 
             var greeting = JsonSerializer.Deserialize<Greeting>(greetingJsonBlob);
 
-            var streamWriter = new StreamWriter(greetingCsvBlob);
+            var streamWriter = new StreamWriter(greetingXmlBlob);
             streamWriter.WriteLine("id;from;to;message;timestamp");
             streamWriter.WriteLine($"{greeting.Id};{greeting.From};{greeting.To};{greeting.Message};{greeting.Timestamp}");
 
             await streamWriter.FlushAsync();
-
         }
     }
 }
