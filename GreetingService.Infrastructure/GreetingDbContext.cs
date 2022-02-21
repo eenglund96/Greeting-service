d00c51670d7ac84c422f1400de359806082be6db
Englund Emelie (BT) <emelie.englund@hm.com>
2022-02-21 13:31:30 +0100

    Added GreetingDbContext class

---
+++ GreetingService.Infrastructure/GreetingDbContext.cs
@@ -0,0 +1,28 @@
+﻿using GreetingService.API.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingService.Infrastructure
+{
+    public class GreetingDbContext : DbContext
+    {
+        public GreetingDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public GreetingDbContext()
+        {
+        }
+
+        public DbSet<Greeting> Greetings { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("GreetingDbConnectionString"));
+        }
+    }
+}
